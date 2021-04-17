a3381025676bc775f3a79f355f7a17770cb942d5
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-04-17 18:16:50 +0300

    +adding: Basic Mountain key system +refactoring: YunusEmre Scene

---
+++ Assets/Scripts/Player/Mountain/Key.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Player.Mountain
+{
+    public class Key : MonoBehaviour
+    {
+        [HideInInspector]public MountainKeyWork mountainKeyWork;
+        public TMP_Text keyTMP;
+        
+        [SerializeField] private float timeMaxPoint;
+        [SerializeField] private Image image;
+        private float _time;
+
+        private void Start()
+        {
+            _time = timeMaxPoint;
+        }
+
+        private void Update()
+        {
+            if (_time > 0)
+            {
+                _time -= Time.deltaTime;
+                //fillImg.fillAmount = _time / timeMaxPoint;
+
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Mountain/MountainKeyWork.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Player.Mountain
+{
+    public class MountainKeyWork : MonoBehaviour
+    {
+
+        private readonly List<string> _keys = new List<string>();
+        [SerializeField] private Key prefabText;
+        [SerializeField] private List<Transform> placeList;
+        private TMP_Text _currentKey;
+
+        private void Start()
+        {
+            _keys.Add("a");
+            _keys.Add("b");
+            _keys.Add("c");
+            _keys.Add("d");
+            _keys.Add("e");
+            _keys.Add("f");
+            _keys.Add("g");
+            _keys.Add("h");
+            
+            SpawnKey();
+        }
+
+        private void Update()
+        {
+            if (!Input.anyKeyDown || Input.GetMouseButtonDown(0)) return;
+            
+            if (_currentKey.text==Input.inputString)
+            {
+                //Character UP
+                Destroy(_currentKey.gameObject);
+                SpawnKey();
+            }
+            else
+            {
+                //Character down
+                Debug.Log("Meeh, you are so bad!");
+            }
+
+        }
+
+        public void SpawnKey()
+        {
+            var keyIndex = Random.Range(0, _keys.Count);
+            var placeIndex = Random.Range(0, placeList.Count);
+            
+            var text = Instantiate(prefabText, transform);
+            
+            text.keyTMP.rectTransform.position = placeList[placeIndex].transform.position;
+            text.keyTMP.text = _keys[keyIndex];
+
+            text.mountainKeyWork = this;
+
+            _currentKey = text.keyTMP;
+        }
+    }
+}
