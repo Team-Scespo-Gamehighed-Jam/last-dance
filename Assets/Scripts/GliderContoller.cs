7152658644de930e4c0d04aa07a842a0057a9ef8
M Serdar Oygen <mserdaroygen@gmail.com>
2021-04-17 14:14:51 +0300

    improve movement (damping downwards)

--- Assets/Scripts/GliderContoller.cs
+++ Assets/Scripts/GliderContoller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GliderContoller : MonoBehaviour
@@ -8,11 +9,17 @@ public class GliderContoller : MonoBehaviour
     private Rigidbody2D _rb;
 
     public float speed;
+    
+    private Vector2 _velocityVector;
+    private Vector2 _dampVelocity;
+    private Vector2 _targetVector;
 
     // Start is called before the first frame update
     void Start()
     {
-        _rb.velocity = Vector2.right * Time.deltaTime * speed;
+        _rb.velocity = _velocityVector;
+        _dampVelocity = Vector2.zero;
+        _velocityVector = Vector2.right * Time.deltaTime * speed;
     }
 
     // Update is called once per frame
@@ -21,11 +28,16 @@ public class GliderContoller : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log("yeeet");
-            _rb.velocity = Vector2.right * Time.deltaTime * speed + (4) * Vector2.up * Time.deltaTime * speed;
+            _rb.velocity = Vector2.right * Time.deltaTime * speed + (6) * Vector2.up * Time.deltaTime * speed;
         }
         else if(Input.GetKeyUp(KeyCode.Space))
         {
-            _rb.velocity = Vector2.right * Time.deltaTime * speed + (-3) * Vector2.up * Time.deltaTime * speed;
+            _targetVector = Vector2.right * Time.deltaTime * speed + (-3) * Vector2.up * Time.deltaTime * speed;
+            
+            _rb.velocity = Vector2.SmoothDamp(_velocityVector, _targetVector, ref _dampVelocity, 1);
+            
+            _velocityVector = _targetVector;
+            _dampVelocity = Vector2.zero;
         }
     }
 }
