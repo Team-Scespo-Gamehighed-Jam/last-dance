808ff5a2c0a34932de7aaa2792045afe570eafb2
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-04-17 12:29:49 +0300

    +adding: Basic Sound System

---
+++ Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sound
+{
+    public class SoundController : MonoBehaviour
+    {
+        private static SoundController SoundPlayer;
+
+        public AudioSource MainAudioSource;
+        
+        private void Awake()
+        {
+            if (SoundPlayer!=null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                SoundPlayer = this;
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
