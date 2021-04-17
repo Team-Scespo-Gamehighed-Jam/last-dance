18619d711dda60d968ca97ad862a9ae6525a2177
Uzay Adıgüzel <uzay.adiguzel@hotmail.com>
2021-04-18 00:23:46 +0300

    + Start Scene + UI + 8Bit Font

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Button PlayButton;
+    public Button CreditsButton;
+    public TMP_Text CreditsText;
+    public TMP_Text LastDance;
+    public Image BottomLine;
+    
+    void Start()
+    {
+        Button creditsButton = CreditsButton.GetComponent<Button>();
+        creditsButton.onClick.AddListener(TaskOnClick);
+        
+        Button playButton = PlayButton.GetComponent<Button>();
+        playButton.onClick.AddListener(PlayGameSceneManager);
+        
+        CreditsText.gameObject.SetActive(false);
+
+    }
+    void PlayGameSceneManager()
+    {
+        Debug.Log("Burdan başlıcak. Tek sahnede nasıl yapıcaz? :D");
+    }
+    void TaskOnClick()
+    {
+        LastDance.gameObject.SetActive(false);
+        BottomLine.gameObject.SetActive(false);
+        CreditsText.gameObject.SetActive(true);
+        PlayButton.gameObject.SetActive(false);
+    }
+}
