ab52264728cb7e03ca87c383da28dabe7aed4494
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-04-17 13:12:02 +0300

    +adding: Basic Input Boolean System, Input Manager

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Input_System;
+using UnityEngine;
+
+namespace Managers
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private List<InputBooleanData> inputBooleanDataList;
+
+        private void Update()
+        {
+            foreach (var i in inputBooleanDataList)
+            {
+                i.Process();
+            }
+        }
+    }
+}
