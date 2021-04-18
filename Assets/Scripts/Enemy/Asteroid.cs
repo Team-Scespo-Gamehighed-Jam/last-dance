259c1f97233a7a5335013e506916bfc28c48e265
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-04-18 17:16:45 +0300

    +refactoring: Rocket Scene

--- Assets/Scripts/Enemy/Asteroid.cs
+++ Assets/Scripts/Enemy/Asteroid.cs
@@ -1,19 +1,39 @@
 ﻿using System;
+using Dialog;
 using Player.Rocket;
 using UnityEngine;
 
 namespace Enemy
 {
+    
     public class Asteroid : MonoBehaviour
     {
+        [SerializeField] private Dialog.Dialog _dialog;
         private void OnTriggerEnter2D(Collider2D other)
         {
+            Debug.Log(other.name);
             var instanceID = other.GetInstanceID();
             if (!RocketHelper.RocketList.ContainsKey(instanceID))
                 return;
             
-            Debug.Log("Game Over!");
-            //TODO: Rocket Game Over!
+            
+            GameOver();
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log(other.collider.name);
+            var instanceID = other.collider.GetInstanceID();
+            if (!RocketHelper.RocketList.ContainsKey(instanceID))
+                return;
+            
+            GameOver();
+        }
+
+        private void GameOver()
+        {
+            DialogManager.instance.ShowDialog(_dialog);
+            LevelLoader.intance.LoadNextLevel(0);
         }
     }
 }
